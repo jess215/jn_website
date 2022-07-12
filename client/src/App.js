7806315a11140f0ac83c3744ee9f44cd9c801987
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-07-11 19:41:51 -0600

    routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import NavBar from './components/shared/NavBar'
+import { Routes, Route, useParams } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
 function App() {
   return (
-    <>
+    <div>
       <div>
-        <h1>Bright Fox Dev</h1>
-        <h2>Jessica Nguyen</h2>
+        <NavBar />
+        <>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </>
       </div>
-    </>
+    </div>
   )
 }
 
