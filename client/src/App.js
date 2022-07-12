4139d5d1e5090751ef634d77eadc56d4b6bc6bd6
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-07-11 19:11:43 -0600

    front end changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <>
+      <div>
+        <h1>Bright Fox Dev</h1>
+        <h2>Jessica Nguyen</h2>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
